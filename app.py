f288b3c13550bab85504d752dd0bdb152ddb445a
aeroafril <semarg53@gmail.com>
2026-01-29 18:11:05 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+from main import scrape_amazon
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    keyword = ""
+    hasil = []
+
+    if request.method == 'POST':
+        keyword = request.form.get('keyword', '').strip()
+        if keyword:
+            hasil = scrape_amazon(keyword)
+
+    return render_template('index.html', hasil=hasil, keyword=keyword)
+
+if __name__ == '__main__':
+    app.run(debug=True)
